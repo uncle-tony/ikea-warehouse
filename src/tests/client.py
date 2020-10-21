a58bd9e81ac93517fe5495173add3f4b05b5f951
g1zm0 <german.retamosa@gmail.com>
2020-10-21 21:03:44 +0000

    Linting tests files and update travisCI

--- src/tests/client.py
+++ src/tests/client.py
@@ -1,52 +1,58 @@
 from tests.base import BaseCase
 from werkzeug.datastructures import FileStorage
 
-import io, os
+import os
+
 
 class TestClient(BaseCase):
     def test_client_v1(self):
         """ Check endpoint availability """
-        f = FileStorage (
-            stream = open(os.path.join("tests/data/inventory.json"), 'rb'),
-            filename = 'inventory.json',
-            content_type = 'application/json'
+        f = FileStorage(
+            stream=open(os.path.join("tests/data/inventory.json"), 'rb'),
+            filename='inventory.json',
+            content_type='application/json'
         )
-        self.app.post('/v.1/article', data = {
-            'file': f 
-        }, content_type='multipart/form-data')
-        f = FileStorage (
-            stream = open(os.path.join("tests/data/products.json"), 'rb'),
-            filename = 'products.json',
-            content_type = 'application/json'
-        )
-        self.app.post('/v.1/product', data = {
-            'file': f 
-        }, content_type='multipart/form-data')
-        response = self.app.post('/v.1/client', data = '''{
-            "action": "sell",
-            "product": "Dining Chair"
-        }''', content_type='application/json')
+        self.app.post(
+            '/v.1/article',
+            data={'file': f},
+            content_type='multipart/form-data')
+        f = FileStorage(
+            stream=open(os.path.join("tests/data/products.json"), 'rb'),
+            filename='products.json',
+            content_type='application/json')
+        self.app.post(
+            '/v.1/product',
+            data={'file': f},
+            content_type='multipart/form-data')
+        response = self.app.post(
+            '/v.1/client',
+            data='''{
+                "action": "sell",
+                "product": "Dining Chair"
+            }''',
+            content_type='application/json')
         self.assertEqual(200, response.status_code)
-    
+
     def test_client_v2(self):
         """ Check endpoint availability """
-        f = FileStorage (
-            stream = open(os.path.join("tests/data/inventory.json"), 'rb'),
-            filename = 'inventory.json',
-            content_type = 'application/json'
+        f = FileStorage(
+            stream=open(os.path.join("tests/data/inventory.json"), 'rb'),
+            filename='inventory.json',
+            content_type='application/json'
         )
-        self.app.post('/v.2/article', data = {
-            'file': f 
-        }, content_type='multipart/form-data')
-        f = FileStorage (
-            stream = open(os.path.join("tests/data/products.json"), 'rb'),
-            filename = 'products.json',
-            content_type = 'application/json'
-        )
-        self.app.post('/v.2/product', data = {
-            'file': f 
-        }, content_type='multipart/form-data')
-        response = self.app.post('/v.2/client', data = '''{
+        self.app.post(
+            '/v.2/article',
+            data={'file': f},
+            content_type='multipart/form-data')
+        f = FileStorage(
+            stream=open(os.path.join("tests/data/products.json"), 'rb'),
+            filename='products.json',
+            content_type='application/json')
+        self.app.post(
+            '/v.2/product',
+            data={'file': f},
+            content_type='multipart/form-data')
+        response = self.app.post('/v.2/client', data='''{
             "action": "sell",
             "product": "Dining Chair"
         }''', content_type='application/json')
